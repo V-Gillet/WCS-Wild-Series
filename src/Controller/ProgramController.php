6507cf5d8df926c889d39952ced9ae0908e353ed
V-Gillet <gillet.valentin45000@gmail.com>
2022-11-28 12:34:15 +0100

    quest-03

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProgramController extends AbstractController
+{
+    #[Route('/program/', name: 'program_index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+
+            'website' => 'Wild Series',
+
+        ]);
+    }
+}
